0ba25787eeee2725db5b586c5f820e3f163ce7c5
Carlos Hernandez <9@carlosjulian.com>
2013-04-19 14:06:55 -0700

    Forma de registro en Top Panel

--- wp-content/plugins/wp-sliding-logindashboard-panel/js/slide.js
+++ wp-content/plugins/wp-sliding-logindashboard-panel/js/slide.js
@@ -1,18 +1,23 @@
 jQuery(document).ready(function() {
 	
 	// Expand Panel
-	jQuery("#open").click(function(){
-		jQuery("div#panel").slideDown("slow");
+	jQuery(".open").click(function(){
+		jQuery("div#panel").slideDown("fast");
+			jQuery("#top ul").css("z-index", "1000");
+		
 	});	
 	
 	// Collapse Panel
-	jQuery("#close").click(function(){
-		jQuery("div#panel").slideUp("slow");
+	jQuery(".close").click(function(){
+		jQuery("div#panel").slideUp("fast", function(){
+			jQuery("#top ul").css("z-index", "6000");
+		});
+		
 	});		
-	
 	// Switch buttons from "Log In | Register" to "Close Panel" on click
 	jQuery("#toggle a").click(function () {
 		jQuery("#toggle a").toggle();
+		
 	});		
 		
 });
--- wp-content/plugins/wp-sliding-logindashboard-panel/wp-sliding-login-dashboard-panel.php
+++ wp-content/plugins/wp-sliding-logindashboard-panel/wp-sliding-login-dashboard-panel.php
@@ -45,7 +45,7 @@ function add_wp_sliding_login_form() {
                             echo $wp_sliding_panel_li_content ;
                         } else { ?>
 			<div class="left border">
-				<h1><?php _e('Welcome'); ?> <?php echo $user_identity ?></h1>
+				<h1><?php _e('Bienvenid@'); ?> <?php echo $user_identity ?></h1>
 				<ul>					
 					<li><a href="<?php bloginfo('url') ?>/wp-admin/index.php"><?php _e('Dashboard') ?></a></li>
 				</ul>
@@ -54,16 +54,16 @@ function add_wp_sliding_login_form() {
 				<h2><?php _e('My Account')?></h2>
 				<ul>					
 					<li><a href="<?php bloginfo('url') ?>/wp-admin/index.php"><?php _e('Dashboard') ?></a></li>
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/profile.php"><?php _e('Profile')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/profile.php"><?php _e('Perfil')?></a></li>
 					<?php if ( current_user_can('level_1') ) : ?>
-						<li><a href="<?php bloginfo('url') ?>/wp-admin/edit-comments.php"><?php _e('Comments')?></a></li>
+						<li><a href="<?php bloginfo('url') ?>/wp-admin/edit-comments.php"><?php _e('Comentarios')?></a></li>
 					<?php endif ?>
-	        		<li><a href="<?php echo wp_logout_url(get_permalink()); ?>" rel="nofollow" title="<?php _e('Log out'); ?>"><?php _e('Log out'); ?></a></li>
+	        		<li><a href="<?php echo wp_logout_url(get_permalink()); ?>" rel="nofollow" title="<?php _e('Log out'); ?>"><?php _e('Cerrar sesión'); ?></a></li>
 				</ul>	
 				<?php if ( current_user_can('level_10') ) : ?>		
 				<h2><?php _e('Appearance')?></h2>
 				<ul>						
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/themes.php"><?php _e('Themes')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/themes.php"><?php _e('Temas')?></a></li>
 					<li><a href="<?php bloginfo('url') ?>/wp-admin/widgets.php"><?php _e('Widgets')?></a></li>
 					<li><a href="<?php bloginfo('url') ?>/wp-admin/theme-editor.php"><?php _e('Editor')?></a></li>
 				</ul>
@@ -73,18 +73,18 @@ function add_wp_sliding_login_form() {
 			<div class="left narrow">			
 				<h2><?php _e('Post')?></h2>				
 				<ul>					
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/post-new.php"><?php _e('New Post')?></a></li>
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/edit.php"><?php _e('Edit Posts')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/post-new.php"><?php _e('Nuevo Post')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/edit.php"><?php _e('Editar Posts')?></a></li>
 				<?php if ( current_user_can('level_3') ) : ?>
 					<li><a href="<?php bloginfo('url') ?>/wp-admin/edit-tags.php"><?php _e('Tags')?></a></li>
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/categories.php"><?php _e('Categories')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/categories.php"><?php _e('Categorías')?></a></li>
 				<?php endif ?>
 				</ul>
 				<?php if ( current_user_can('level_10') ) : ?>		
 				<h2><?php _e('Plugins')?></h2>				
 				<ul>						
 					<li><a href="<?php bloginfo('url') ?>/wp-admin/plugins.php"><?php _e('Plugins')?></a></li>
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/plugin-install.php"><?php _e('Installed')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/plugin-install.php"><?php _e('Instalados')?></a></li>
 					<li><a href="<?php bloginfo('url') ?>/wp-admin/plugin-editor.php"><?php _e('Editor')?></a></li>
 				</ul>
 				<?php endif ?>
@@ -95,20 +95,20 @@ function add_wp_sliding_login_form() {
 				<?php if ( current_user_can('level_3') ) : ?>	
 				<h2><?php _e('Pages')?></h2>				
 				<ul>		
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/post-new.php"><?php _e('New Page')?></a></li>
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/edit-pages.php"><?php _e('Edit')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/post-new.php"><?php _e('Nueva Página')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/edit-pages.php"><?php _e('Editar')?></a></li>
 				</ul>
 				<?php endif ?>			
 				<h2><?php _e('Media')?></h2>				
 				<ul>					
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/upload.php"><?php _e('Library')?></a></li>
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/media-new.php"><?php _e('Add New')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/upload.php"><?php _e('Librería')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/media-new.php"><?php _e('Agregar Nuevo')?></a></li>
 				</ul>
 				<?php if ( current_user_can('level_3') ) : ?>		
 				<h2><?php _e('Users')?></h2>
 				<ul>						
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/users.php"><?php _e('Authors &amp; Users')?></a></li>
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/user-new.php"><?php _e('Add New')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/users.php"><?php _e('Autores &amp; Usuarios')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/user-new.php"><?php _e('Agregar Nuevo')?></a></li>
 				</ul>
 				<?php endif ?>
 			</div>
@@ -118,13 +118,13 @@ function add_wp_sliding_login_form() {
 				<h2><?php _e('Settings')?></h2>
 				<ul>						
 					<li><a href="<?php bloginfo('url') ?>/wp-admin/options-general.php"><?php _e('General')?></a></li>
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/options-writing.php"><?php _e('Writing')?></a></li>
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/options-reading.php"><?php _e('Reading')?></a></li>
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/options-discussion.php"><?php _e('Discussion')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/options-writing.php"><?php _e('Escribir')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/options-reading.php"><?php _e('Leer')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/options-discussion.php"><?php _e('Discusiones')?></a></li>
 					<li><a href="<?php bloginfo('url') ?>/wp-admin/options-media.php"><?php _e('Media')?></a></li>
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/options-privacy.php"><?php _e('Privacy')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/options-privacy.php"><?php _e('Privacidad')?></a></li>
 					<li><a href="<?php bloginfo('url') ?>/wp-admin/options-permalink.php"><?php _e('Permalinks')?></a></li>
-					<li><a href="<?php bloginfo('url') ?>/wp-admin/options-misc.php"><?php _e('Miscellaneous')?></a></li>
+					<li><a href="<?php bloginfo('url') ?>/wp-admin/options-misc.php"><?php _e('Misceláneo')?></a></li>
 				</ul>
 			</div>
 			<?php endif ?>
@@ -135,13 +135,15 @@ function add_wp_sliding_login_form() {
     <!-- The tab on top -->	
 	<div class="tab">
 		<ul class="login">
+	    	<li class="left">&nbsp;</li>
 	    	<!-- Logout -->
-	        <li><a href="<?php echo wp_logout_url(get_permalink()); ?>" rel="nofollow" title="<?php _e('Log out'); ?>"><?php _e('Log out'); ?></a></li>
-			<li class="sep">|</li>
+<!--	        <li><a href="<?php echo wp_logout_url(get_permalink()); ?>" rel="nofollow" title="<?php _e('Log out'); ?>"><?php _e('Log out'); ?></a></li>
+			<li class="sep">|</li>-->
 			<li id="toggle">
-				<a id="open" class="open" href="#"><?php if ($wp_sliding_panel_replace_default) echo $wp_sliding_panel_li_open_label; else _e('Panel');?></a>
-				<a id="close" style="display: none;" class="close" href="#"><?php if ($wp_sliding_panel_replace_default) echo $wp_sliding_panel_li_close_label; _e('Close');?></a>
+				<a id="open" class="open" href="#"><?php if ($wp_sliding_panel_replace_default) echo $wp_sliding_panel_li_open_label; else _e('Mi cuenta');?></a>
+				<a id="close" style="display: none;" class="close" href="#"><?php if ($wp_sliding_panel_replace_default) echo $wp_sliding_panel_li_close_label; _e('Cerrar');?></a>
 			</li>
+	    	<li class="right">&nbsp;</li>
 		</ul> 
 	</div> <!-- / top -->
 <?php
@@ -155,39 +157,39 @@ function add_wp_sliding_login_form() {
                     } else {
                         ?>
 			<div class="left border">
-				<h1><?php bloginfo('name') ?> : <?php _e('Welcome') ?> !</h1>
+				<h1>¡<?php _e('Bienvenido a ') ?><?php bloginfo('name') ?>!</h1>
 			</div>
 			<div class="left">
 				<!-- Login Form -->
 				<form class="clearfix" action="<?php bloginfo('url') ?>/wp-login.php" method="post">
-					<h1><?php _e('Authorize') ?></h1>
-					<label class="grey" for="log"><?php _e('Login Name') ?> :</label>
+					<h1><?php _e('Inicia Sesión') ?></h1>
+					<label class="grey" for="log"><?php _e('Ususario:') ?></label>
 					<input class="field" type="text" name="log" id="log" value="<?php echo wp_specialchars(stripslashes($user_login), 1) ?>" size="23" />
-					<label class="grey" for="pwd"><?php _e('Password:') ?></label>
+					<label class="grey" for="pwd"><?php _e('Contraseña:') ?></label>
 					<input class="field" type="password" name="pwd" id="pwd" size="23" />
-	            	<label><input name="rememberme" id="rememberme" type="checkbox" checked="checked" value="forever" />&nbsp;<?php _e('Remember Me') ?></label>
+	            	<label><input name="rememberme" id="rememberme" type="checkbox" checked="checked" value="forever" />&nbsp;<?php _e('Recuérdame') ?></label>
         			<div class="clear"></div>
-					<input type="submit" name="submit" value="Login" class="bt_login" />
+					<input type="submit" name="submit" value="Entrar" class="bt_login" />
 					<input type="hidden" name="redirect_to" value="<?php echo $_SERVER['REQUEST_URI']; ?>"/>
-					<a class="lost-pwd" href="<?php bloginfo('url') ?>/wp-login.php?action=lostpassword"><?php _e('Lost Password') ?></a>
+					<a class="lost-pwd" href="<?php bloginfo('url') ?>/wp-login.php?action=lostpassword"><?php _e('¿Olvidaste tu contraseña?') ?></a>
 				</form>
 			</div>
 			<div class="left right">
 			<?php if (get_option('users_can_register')) : ?>
 				<!-- Register Form -->
 				<form name="registerform" id="registerform" action="<?php echo site_url('wp-login.php?action=register', 'login_post') ?>" method="post">
-					<h1><?php _e('Register') ?></h1>
-					<label class="grey" for="user_login"><?php _e('Username') ?></label>
+					<h1><?php _e('Regístrate') ?></h1>
+					<label class="grey" for="user_login"><?php _e('Usuario') ?></label>
 					<input class="field" type="text" name="user_login" id="user_login" class="input" value="<?php echo attribute_escape(stripslashes($user_login)); ?>" size="20" tabindex="10" />
 					<label class="grey" for="user_email"><?php _e('E-mail') ?></label>
 					<input class="field" type="text" name="user_email" id="user_email" class="input" value="<?php echo attribute_escape(stripslashes($user_email)); ?>" size="25" tabindex="20" />
 					<?php do_action('register_form'); ?>
-					<label id="reg_passmail"><?php _e('A password will be e-mailed to you.') ?></label>
-					<input type="submit" name="wp-submit" id="wp-submit" value="<?php _e('Register'); ?>" class="bt_register" />
+					<label id="reg_passmail"><?php _e('Se te enviará una contraseña a tu e-mail.') ?></label>
+					<input type="submit" name="wp-submit" id="wp-submit" value="<?php _e('Registrarse'); ?>" class="bt_register" />
 				</form>
 			<?php else : ?>
-				<h1><?php _e('Register') ?></h1>
-				<p><?php _e('Please contact the administrator.') ?></p>
+				<h1><?php _e('Regístrate') ?></h1>
+				<p><?php _e('Por favor contacta al administrador.') ?></p>
 			<?php endif ?>
 			</div>
                     <?php } ?>
@@ -197,11 +199,13 @@ function add_wp_sliding_login_form() {
     <!-- The tab on top -->
 	<div class="tab">
 		<ul class="login">
+	    	<li class="left">&nbsp;</li>
 	    	<!-- Login / Register -->
 			<li id="toggle">
-				<a id="open" class="open" href="#"><?php  if ($wp_sliding_panel_replace_default) echo $wp_sliding_panel_lo_open_label; else _e('Log In'); ?></a>
-				<a id="close" style="display: none;" class="close" href="#"><?php  if ($wp_sliding_panel_replace_default) echo $wp_sliding_panel_lo_close_label; else _e('Close'); ?></a>
+				<a id="open" class="open" href="#"><?php  if ($wp_sliding_panel_replace_default) echo $wp_sliding_panel_lo_open_label; else _e('Mi Cuenta'); ?></a>
+				<a id="close" style="display: none;" class="close" href="#"><?php  if ($wp_sliding_panel_replace_default) echo $wp_sliding_panel_lo_close_label; else _e('Cerrar'); ?></a>
 			</li>
+	    	<li class="right">&nbsp;</li>
 		</ul>
 	</div> <!-- / top -->
 <?php } ?>
